b343af8ab8f7e4edff3a71ff0e680d573d5ccd2c
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2016-04-13 06:54:01 +0200

    Solved the Time To Eat problem.

---
+++ Easy/Time To Eat.py
@@ -0,0 +1,9 @@
+import sys
+
+with open(sys.argv[1], 'r') as test_cases:
+	for test in test_cases:
+		if test == '':
+			continue
+
+		timestamps = test.strip().split(' ')
+		print(' '.join(sorted(timestamps, reverse=True)))
