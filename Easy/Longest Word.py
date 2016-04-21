f5e0deea00bb150eececccdafd63a666a9ea7d2b
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2016-04-21 09:26:32 +0200

    Solved the Longest Word problem.

---
+++ Easy/Longest Word.py
@@ -0,0 +1,9 @@
+import sys
+
+with open(sys.argv[1], 'r') as test_cases:
+    for test in test_cases:
+        if test == '':
+            continue
+
+        words = test.strip().split(' ')
+        print(max(words, key=len))
