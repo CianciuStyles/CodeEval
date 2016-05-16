dee40940dd5679973d47f634f23e02ed7f96591e
Stefano Cianciulli <stefano.cianciulli@gmail.com>
2016-05-16 16:46:01 +0200

    Solved the Mersenne Prime problem.

--- Easy/Mersenne Prime.py
+++ Easy/Mersenne Prime.py
@@ -1,25 +1,11 @@
-import math
 import sys
 
-def is_prime(num):
-	for div in range(2, int(math.sqrt(num)) + 1):
-		if num % div == 0:
-			return False
-
-	return True
+mersenne_numbers = [3, 7, 31, 127, 2047]
 
 with open(sys.argv[1], 'r') as test_cases:
 	for test in test_cases:
-		if test == "":
+		if test == '':
 			continue
 
-		limit = int(test)
-		num, p, mersenne_primes = 3, 2, []
-		while num < limit:
-			num = 2 ** p - 1
-			if is_prime(num) is True:
-				mersenne_primes.append(str(num))
-
-			p += 1
-
-		print(", ".join(mersenne_primes))
+		less_than_test = list(map(str, filter(lambda x: x < int(test), mersenne_numbers)))
+		print(', '.join(less_than_test))
